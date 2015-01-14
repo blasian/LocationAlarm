0d6064625c10ef662b10fbfd7f441aa27bd7e83e
blasian <mikespearman.e@gmail.com>
2015-01-14 15:16:11 -0800

    Initial Commit

---
+++ LocationAlarm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocationAlarm
+//
+//  Created by Michael Spearman on 1/14/15.
+//  Copyright (c) 2015 Michael Spearman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocationAlarm/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LocationAlarm
+//
+//  Created by Michael Spearman on 1/14/15.
+//  Copyright (c) 2015 Michael Spearman. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
